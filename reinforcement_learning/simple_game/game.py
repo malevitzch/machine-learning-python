89202f493f6eddd4c6c5b4ed4577bef8585d1780
malevitzch <malevitzch@gmail.com>
2025-04-20 16:24:33 +0200

    Added a simple display to the game

--- reinforcement_learning/simple_game/game.py
+++ reinforcement_learning/simple_game/game.py
@@ -1,6 +1,7 @@
 # Turns are coded in 1/8ths of a "real" turn
 
 from enum import Enum
+import pygame
 
 
 class Tile(Enum):
@@ -12,4 +13,31 @@ class Tile(Enum):
 class Game:
     def __init__(self, player):
         self.player = player
-        self.grid = [[None for _ in range(7)] for _ in range(7)]
+        self.grid = [[Tile.NOTHING for _ in range(7)] for _ in range(7)]
+        self.grid[3][3] = Tile.PLAYER
+
+
+player = 0
+game = Game(player)
+
+screen = pygame.display.set_mode((7 * 64, 7 * 64))
+pygame.display.set_caption("The Glorious Display")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    for i in range(7):
+        for j in range(7):
+            width = 64
+            height = 64
+            if game.grid[i][j] == Tile.NOTHING:
+                color = (255, 255, 255)
+            else:
+                color = (0, 0, 0)
+            pygame.draw.rect(screen, color,
+                             (i * width, j * height,
+                                 (i + 1) * width, (j + 1) * height))
+    pygame.display.flip()
+pygame.quit()
