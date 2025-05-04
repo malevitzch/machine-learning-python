c6b8ad45ddb3c2b5831dd0f013381c4be0200a90
malevitzch <malevitzch@gmail.com>
2025-05-04 10:49:40 +0200

    Spilt the data into a training set and a verification one

--- kaggle_datasets/phishing/analyzer.py
+++ kaggle_datasets/phishing/analyzer.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import sys
 try:
-    data = pd.read_csv('email_phishing_data.csv')
+    df = pd.read_csv('email_phishing_data.csv')
 except FileNotFoundError:
     print("Cannot find the dataset 'email_phishing_data.csv'")
     sys.exit(1)
 
-print(data.shape)
+training_df = df.sample(frac=0.8, random_state=42).reset_index(drop=True)
+verification_df = df.drop(training_df.index).reset_index(drop=True)
+
+
+print(training_df.shape)
+print(verification_df.shape)
