637cce92be91c9ddcbd13502f4cc7bf9d5fedd78
malevitzch <malevitzch@gmail.com>
2025-05-06 13:40:42 +0200

    Fixed a bug in the splitting function

--- kaggle_datasets/phishing/analyzer.py
+++ kaggle_datasets/phishing/analyzer.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import IsolationForest
 
 def split_df(df, percentage, random_state=42):
     training_df = df.sample(
-        frac=0.8, random_state=42).reset_index(drop=True)
+        frac=percentage, random_state=random_state).reset_index(drop=True)
     verification_df = df.drop(training_df.index).reset_index(drop=True)
     return training_df, verification_df
 
