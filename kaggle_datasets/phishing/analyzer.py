2b2696ddfb46328a28ee1f4387021c13006226a7
malevitzch <malevitzch@gmail.com>
2025-05-06 09:05:48 +0200

    The program now extends the clusters from the training data to the verification data in order to make predictions (it has 93% accuracy)

--- kaggle_datasets/phishing/analyzer.py
+++ kaggle_datasets/phishing/analyzer.py
@@ -7,17 +7,31 @@ from sklearn.cluster import KMeans
 def cluster_test(df):
     kmeans = KMeans(n_clusters=2, random_state=42)
 
-    kmeans.fit(df.iloc[:, :-1])
+    training_df = df.sample(frac=0.8, random_state=42).reset_index(drop=True)
+    verification_df = df.drop(training_df.index).reset_index(drop=True)
 
-    df['cluster'] = kmeans.labels_
-    safe_cluster = df['cluster'].mode()[0]
+    kmeans.fit(training_df.iloc[:, :-1])
+
+    training_df['cluster'] = kmeans.labels_
+    safe_cluster = training_df['cluster'].mode()[0]
     if safe_cluster != 0:
-        df['cluster'] = df['cluster'].map({0: 1, 1: 0})
-    correct = (df['label'] == df['cluster']).sum()
-    all = len(df)
+        training_df['cluster'] = training_df['cluster'].map({0: 1, 1: 0})
+
+    correct = (training_df['label'] == training_df['cluster']).sum()
+    all = len(training_df)
 
     percent = int((correct / all) * 100)
-    print(f"{correct}/{all}: {percent}%")
+    print(f"Training dataset: {correct}/{all}: {percent}%")
+
+    verification_df['cluster'] = kmeans.predict(verification_df.iloc[:, :-1])
+    if safe_cluster != 0:
+        verification_df['cluster'] = (
+            verification_df['cluster'].map({0: 1, 1: 0}))
+    correct_ver = (verification_df['label'] ==
+                   verification_df['cluster']).sum()
+    all_ver = len(verification_df)
+    percent_ver = int((correct_ver / all_ver) * 100)
+    print(f"Verification dataset: {correct_ver}/{all_ver}: {percent_ver}%")
 
 
 try:
@@ -26,13 +40,7 @@ except FileNotFoundError:
     print("Cannot find the dataset 'email_phishing_data.csv'")
     sys.exit(1)
 
-training_df = df.sample(frac=0.8, random_state=42).reset_index(drop=True)
-verification_df = df.drop(training_df.index).reset_index(drop=True)
-
-
-print(training_df.shape)
-print(verification_df.shape)
-
 # This method has 98% accuracy and does not require precise labeling of data
+# used to generate the kmeans
 # Rather it requires a vague idea of what the clusters represent
-cluster_test(training_df)
+cluster_test(df)
