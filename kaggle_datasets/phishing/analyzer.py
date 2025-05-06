92d46a3c356b82e2d2ea744008b1463bc721d329
malevitzch <malevitzch@gmail.com>
2025-05-06 10:09:40 +0200

    Faced the bane of statistics

--- kaggle_datasets/phishing/analyzer.py
+++ kaggle_datasets/phishing/analyzer.py
@@ -3,11 +3,15 @@ import sys
 
 from sklearn.cluster import KMeans
 
+# This is very bad for the problem since the clusters are not exclusive
+# very bad idea
+
 
 def cluster_test(df):
-    kmeans = KMeans(n_clusters=2, random_state=42)
+    kmeans = KMeans(n_clusters=2, random_state=42, init='k-means++')
 
-    training_df = df.sample(frac=0.8, random_state=42).reset_index(drop=True)
+    training_df = df.sample(
+        frac=0.8, random_state=42).reset_index(drop=True)
     verification_df = df.drop(training_df.index).reset_index(drop=True)
 
     kmeans.fit(training_df.iloc[:, :-1])
@@ -27,11 +31,29 @@ def cluster_test(df):
     if safe_cluster != 0:
         verification_df['cluster'] = (
             verification_df['cluster'].map({0: 1, 1: 0}))
+
     correct_ver = (verification_df['label'] ==
                    verification_df['cluster']).sum()
     all_ver = len(verification_df)
     percent_ver = int((correct_ver / all_ver) * 100)
     print(f"Verification dataset: {correct_ver}/{all_ver}: {percent_ver}%")
+
+    tp = ((verification_df['cluster'] == 0) & (
+        verification_df['label'] == 0)).sum()
+    fp = ((verification_df['cluster'] == 0) & (
+        verification_df['label'] == 1)).sum()
+    tn = ((verification_df['cluster'] == 1) & (
+        verification_df['label'] == 1)).sum()
+    fn = ((verification_df['cluster'] == 1) & (
+        verification_df['label'] == 0)).sum()
+
+    confusion_table = pd.DataFrame({
+        'Predicted 0': [tp, fp],
+        'Predicted 1': [fn, tn]
+    }, index=['Actual 0', 'Actual 1'])
+
+    print("Confusion Matrix:")
+    print(confusion_table)
 
 
 try:
@@ -40,7 +62,4 @@ except FileNotFoundError:
     print("Cannot find the dataset 'email_phishing_data.csv'")
     sys.exit(1)
 
-# This method has 98% accuracy and does not require precise labeling of data
-# used to generate the kmeans
-# Rather it requires a vague idea of what the clusters represent
 cluster_test(df)
