8dc16de36cbecc655888199a1712c29a048bb197
malevitzch <malevitzch@gmail.com>
2025-05-06 11:54:10 +0200

    The program now provides a bit more information about which test it is running

--- kaggle_datasets/phishing/analyzer.py
+++ kaggle_datasets/phishing/analyzer.py
@@ -9,6 +9,7 @@ from sklearn.ensemble import IsolationForest
 
 
 def cluster_test(df):
+    print("Running the KMeans cluster test")
     kmeans = KMeans(n_clusters=2, random_state=42, init='k-means++')
 
     training_df = df.sample(
@@ -62,6 +63,7 @@ def cluster_test(df):
 # 92% chance for good answer on regular
 def isolation_forest_test(df):
 
+    print("Running the isolation forest test")
     model = IsolationForest(contamination=0.08, random_state=42)
 
     training_df = df.sample(
@@ -92,7 +94,7 @@ def isolation_forest_test(df):
         'Predicted 1': [fp, tp]
     }, index=['Actual 0', 'Actual 1'])
 
-    print("Confusion Matrix (Anomaly = Positive Class):")
+    print("Confusion Matrix (Phishing = 1):")
     print(confusion_table)
     positive_accuracy = (tp*100 // (fn + tp))
     negative_accuracy = (tn*100 // (tn + fp))
