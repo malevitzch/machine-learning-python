3469f4e7ad198c4e07ee946a9310d30365348ad2
malevitzch <malevitzch@gmail.com>
2025-05-09 00:21:22 +0200

    Added some base stuff for avocado price series

---
+++ kaggle_datasets/avocado/describe.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+import sys
+
+try:
+    df = pd.read_csv('avocado.csv')
+except FileNotFoundError:
+    print("Cannot find the dataset 'email_phishing_data.csv'")
+    sys.exit(1)
+
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index('Date', inplace=True)
+
+df.sort_index(inplace=True)
+# df = df.asfreq('D', method='bfill')
+
+plt.figure(figsize=(10, 8))
+corr = df[['AveragePrice', 'Total Volume',
+           'Total Bags', 'Large Bags', 'Small Bags']].corr()
+sns.heatmap(corr, annot=True, cmap='coolwarm', fmt=".2f", square=True)
+plt.tight_layout()
+plt.show()
