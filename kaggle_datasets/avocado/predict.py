ff7c16c14a69cc1bd028b6bb6a1624b2996cdd67
malevitzch <malevitzch@gmail.com>
2025-05-27 20:55:04 +0200

    Moved reading files into a separate function in a way that's much nicer and more usable

--- kaggle_datasets/avocado/predict.py
+++ kaggle_datasets/avocado/predict.py
@@ -3,6 +3,15 @@ import pandas as pd
 import sys
 
 
+def read_df_from_csv(name):
+    try:
+        df = pd.read_csv(name)
+    except FileNotFoundError:
+        print(f'Cannot find the dataset \'{name}\'')
+        sys.exit(1)
+    return df
+
+
 def index_by_dates(df):
     df['Date'] = pd.to_datetime(df['Date'])
     df.set_index('Date', inplace=True)
@@ -11,11 +20,8 @@ def index_by_dates(df):
 
 # Read data and split it into conventional and organic avocado
 # cause as far as I'm concerned they are completely different products
-try:
-    df = pd.read_csv('avocado.csv')
-except FileNotFoundError:
-    print("Cannot find the dataset 'email_phishing_data.csv'")
-    sys.exit(1)
+
+df = read_df_from_csv('avocado.csv')
 
 index_by_dates(df)
 
