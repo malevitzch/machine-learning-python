6cb27bc38e1fb821c6e4d59594ea81da5f899c08
malevitzch <malevitzch@gmail.com>
2025-11-05 11:21:46 +0100

    Upgraded the approximation

--- regression/trig_fitting.py
+++ regression/trig_fitting.py
@@ -4,9 +4,9 @@ import torch as tc
 import matplotlib.pyplot as plt
 
 
-def sin_poly(a, b):
+def sin_poly(a, b, c):
     def fn(x):
-        return a * x + b * (x**3)
+        return a * x + b * (x**3) + c * (x**5)
 
     return fn
 
@@ -14,15 +14,16 @@ def sin_poly(a, b):
 def fit(X, Y, iters=1000, lr=0.10):
     a = tc.tensor(1, dtype=tc.float64, requires_grad=True)
     b = tc.tensor(0, dtype=tc.float64, requires_grad=True)
-    optimizer = tc.optim.SGD([a, b], lr=lr)
+    c = tc.tensor(0, dtype=tc.float64, requires_grad=True)
+    optimizer = tc.optim.Adam([a, b, c], lr=lr)
 
     for i in range(iters):
-        Y_pred = a * X + b * (X**3)
+        Y_pred = sin_poly(a, b, c)(X)
         loss = tc.mean((Y - Y_pred) ** 2)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    return a.detach().numpy(), b.detach().numpy()
+    return a.detach().numpy(), b.detach().numpy(), c.detach().numpy()
 
 
 X = np.array([-pi / 2 + i * pi / 20 for i in range(21)])
@@ -32,10 +33,10 @@ print(Y)
 X_t = tc.tensor(X, dtype=tc.float64)
 Y_t = tc.tensor(Y, dtype=tc.float64)
 
-a, b = fit(X_t, Y_t)
-print(f"{a}*x + {b}*x^3")
+a, b, c = fit(X_t, Y_t)
+print(f"{a}*x + {b}*x^3 + {c}*x^5")
 
-fn = sin_poly(a, b)
+fn = sin_poly(a, b, c)
 xs = np.linspace(-pi / 2, pi / 2, 100)
 ys = fn(xs)
 plt.plot(xs, ys)
